228fcc752f78b7e61175f878a6269d54afa4ad97
Rudie Dirkx, lpt <rudie@hotblocks.nl>
2011-11-23 06:35:57 +0100

    Fcking newlines! Macintosh? Really? W00t?

--- optional_helpers.php
+++ optional_helpers.php
@@ -1,1 +1,9 @@
-<?phpfunction repeat( $amount, Closure $callback, $arguments = array() ) {	for ( $i=0; $i<$amount; $i++ ) {		call_user_func_array($callback, $arguments);	}}
+<?php
+
+function repeat( $amount, Closure $callback, $arguments = array() ) {
+	for ( $i=0; $i<$amount; $i++ ) {
+		call_user_func_array($callback, $arguments);
+	}
+}
+
+
